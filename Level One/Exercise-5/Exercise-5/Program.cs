4eabc13d1ad6d38f3d0499a5ca4294319ea205e8
Robin Haider <robinhaider69@gmail.com>
2019-09-20 23:05:24 +0600

    cm-2 ex 3-6

---
+++ Level One/Exercise-5/Exercise-5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        //Write a C# Sharp program to create a new string where 'if' is added to the front of a given string. If the string already begins with 'if', return the string unchanged.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test("If you are"));
+            Console.WriteLine(test("if whatever"));
+            Console.WriteLine(test("whatever"));
+
+            Console.ReadKey();
+        }
+
+        public static string test(string sentence)
+        {
+            if (sentence.Substring(0, 2).ToLower().Equals("if"))
+            {
+                return sentence;
+            }
+
+            return "If " + sentence;
+        }
+    }
+}
---
+++ Level One/Exercise-6/Exercise-6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        //Write a C# Sharp program to remove the character in a given position of a given string.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(removeWord("Extraordinary",0));
+            Console.WriteLine(removeWord("Extraordinary",12));
+
+            Console.ReadKey();
+        }
+
+        public static string removeWord(string word, int position)
+        {
+            return word.Remove(position, 1);
+        }
+    }
+}
