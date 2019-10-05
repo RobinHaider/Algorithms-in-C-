21d8d16861959b2bb847164afab68eefa302223b
Robin Haider <robinhaider69@gmail.com>
2019-10-06 00:02:33 +0600

    commit-3 ex-7

---
+++ Level One/Exercise-7/Exercise-7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        //Write a C# Sharp program to exchange the first and last characters in a given string and return the new string.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SwapLetter("Raheel"));
+            Console.WriteLine(SwapLetter("ABCD"));
+            Console.WriteLine(SwapLetter("a"));
+            Console.ReadKey();
+        }
+
+
+        public static string SwapLetter(string str)
+        {
+            return str.Length > 1
+                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1)
+                : str;
+        }
+    }
+}
