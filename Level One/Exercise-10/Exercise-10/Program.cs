ea2d4650df6d210379054eb446198058b34fce09
Robin Haider <robinhaider69@gmail.com>
2019-10-07 22:10:10 +0600

    Commit-4 Ex 8-10

---
+++ Level One/Exercise-10/Exercise-10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        //Write a C# Sharp program to check if a given positive number is a multiple of 3 or a multiple of 7
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test(10));
+            Console.WriteLine(Test(18));
+            Console.WriteLine(Test(21));
+            Console.WriteLine(Test(25));
+            Console.ReadKey();
+        }
+
+        public static bool Test(int a)
+        {
+            return a % 3 == 0 || a % 7 == 0;
+        }
+    }
+}
---
+++ Level One/Exercise-9/Exercise-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        //Write a C# Sharp program to create  a new string with the last char added at the front and back of a given string of length 1 or more
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test("Robin"));
+            Console.WriteLine(Test("Raheel"));
+            Console.WriteLine(Test("B"));
+            Console.ReadKey();
+        }
+
+        public static string Test(string str)
+        {
+            var s = str.Substring(0, 1);
+            return s + str + s;
+        }
+    }
+}
